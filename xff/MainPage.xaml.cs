0d14f38c204ee2dd024842480e4fa018ce0e6f05
karl <karl.mellikov@gmail.com>
2024-01-12 11:02:38 +0200

    update

--- xff/MainPage.xaml.cs
+++ xff/MainPage.xaml.cs
@@ -9,7 +9,6 @@ namespace xff
         {
             InitializeComponent();
 
-            // Static coordinates for testing (replace with your actual coordinates)
 
             double latitude = 58.1499;
             double longitude = 22.5055;
@@ -19,13 +18,7 @@ namespace xff
                               $"src='https://www.google.com/maps/embed/v1/view?key={apiKey}&center={latitude.ToString(CultureInfo.InvariantCulture)},{longitude.ToString(CultureInfo.InvariantCulture)}&zoom=18&maptype=satellite'" +
                               $"allowfullscreen></iframe></body></html>";
 
-            // Set the HTML content in the WebView
             googleMapView.Source = new HtmlWebViewSource { Html = htmlContent };
-
-
-            // Set the center parameter in the Google Maps embed URL
-            //var googleMapsEmbedUrl = $"https://www.google.com/maps/embed/v1/view?key=AIzaSyBz1eES6CyIBLu2KAt92nQtmDzYOa9Dsj8&center=-33.8569,151.2152&zoom=18&maptype=satellite";
-            //googleMapView.Source = new UrlWebViewSource { Url = googleMapsEmbedUrl };
+        }
         }
     }
-}
