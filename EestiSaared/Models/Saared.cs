d19381f07303ba26cc58da13008aa643687e7dc4
Siim Koppel <siimk999@gmail.com>
2024-01-24 19:52:17 +0200

    Add Saared models

---
+++ EestiSaared/Models/Saared.cs
@@ -0,0 +1,10 @@
+﻿namespace EestiSaared.Models
+{
+    public  class Saared
+    {
+        public string SaarImage { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
